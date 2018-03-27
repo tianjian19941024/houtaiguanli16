992564a9369216b955044809e85d75d5263d0b86
田晓剑 <1714425242@qq.com>
2018-03-27 22:31:17 +0800

    环境测试

---
+++ src/component/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>{{val}}</div>
+</template>
+
+
+<script>
+  export default{
+       // 这是es6中对象定义函数方法的语法
+       //组件内一个函数返回一个对象的形式
+       data(){
+           return {
+               val:"vue项目脚手架 环境测试"
+           }
+       } 
+  }
+</script> 
+
+<style scoped>
+
+</style>
+
+
+
+
+
